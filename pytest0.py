7d983c227337c70b4da3cf2423ca7d9ea9e21539
sweetybiswas1 <sweety.biswas@cbnits.com>
2022-07-19 12:26:48 +0530

    first commit

---
+++ pytest0.py
@@ -0,0 +1,28 @@
+Initial Commit
+
+# Please enter the commit message for your changes. Lines starting
+# with '#' will be ignored, and an empty message aborts the commit.
+#
+# On branch master
+#
+# Initial commit
+#
+# Changes to be committed:
+#	new file:   pytest0.py
+#
+# Untracked files:
+#	.idea/
+#	__pycache__/
+#	conftest.py
+#	pytest1.py
+#	pytest10.py
+#	pytest2(multiple_test).py
+#	pytest3(grouping).py
+#	pytest4(fixtures).py
+#	pytest5(parametrized).py
+#	pytest6(xfail,skip).py
+#	pytest7(test_api).py
+#	pytest8(check_conftest).py
+#	pytest9(check_conftest2).py
+#
+	
